7e6c22937a22b67d6ab746d54cc1cac82d2ba39d
More3428 <andmoreno@csumb.edu>
2023-05-11 15:19:24 -0700

    Andre

--- Assets/Interactable Assets (Ray's)/Scripts/InteractHandler.cs
+++ Assets/Interactable Assets (Ray's)/Scripts/InteractHandler.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+// using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class InteractHandler : MonoBehaviour
